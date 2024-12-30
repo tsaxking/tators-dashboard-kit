f1e2bbafe47f5c613011c1a09bd91d63207713e0
taylor <taylorking206@u.boisestate.edu>
2024-12-29 22:55:27 -0700

    Squashed 'src/lib/ts-utils/' changes from 1d8058e..2d3aff5
    2d3aff5 add streamer
    
    git-subtree-dir: src/lib/ts-utils
    git-subtree-split: 2d3aff5b65aae73f9bc07855836a5ded0d14b1e1

---
+++ stream.ts
@@ -0,0 +1,72 @@
+import { EventEmitter } from './event-emitter';
+import { attemptAsync } from './check';
+
+type Events<T = unknown> = {
+    data: T;
+    error: Error;
+    end: void;
+}
+
+export class Streamer<T = unknown> {
+    private readonly emitter = new EventEmitter<Events<T>>();
+
+    public on = this.emitter.on.bind(this.emitter);
+    public off = this.emitter.off.bind(this.emitter);
+    public once = this.emitter.once.bind(this.emitter);
+    private emit = this.emitter.emit.bind(this.emitter);
+
+    public add(data: T) {
+        this.emit('data', data);
+    }
+
+    public end() {
+        this.emit('end');
+        this.emitter.destroyEvents();
+    }
+
+    public error(error: Error) {
+        this.emit('error', error);
+        this.emitter.destroyEvents();
+    }
+
+    public pipe(stream: Streamer<T> | ((data: T) => void)) {
+        this.on('data', (data) => {
+            if (stream instanceof Streamer) {
+                stream.add(data);
+            } else {
+                stream(data);
+            }
+        });
+        this.on('end', () => {
+            if (stream instanceof Streamer) {
+                stream.end();
+            }
+        });
+        this.on('error', (error) => {
+            if (stream instanceof Streamer) {
+                stream.error(error);
+            }
+        });
+    }
+
+    public await(timeout = 0) {
+        return attemptAsync(async () => new Promise<T[]>((res, rej) => {
+            const data: T[] = [];
+            let resolved = false;
+            const resolve = (error?: Error) => {
+                if (resolved) return;
+                resolved = true;
+                if (error) return rej(error);
+                res(data);
+            }
+
+            this.on('data', (d) => data.push(d));
+            this.on('end', () => resolve());
+            this.on('error', resolve);
+
+            if (timeout) {
+                setTimeout(() => resolve(new Error('Stream Timeout')), timeout);
+            }
+        }));
+    }
+}
