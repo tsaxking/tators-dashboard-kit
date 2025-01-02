07e26866809a2e2ae472a4abcddb7cb043038384
taylor <taylorking206@u.boisestate.edu>
2025-01-01 17:05:22 -0700

    fix routes and sass

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+    import '$lib/imports';
+
+    const { children } = $props();
+</script>
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Card from "$lib/components/bootstrap/Card.svelte";
+</script>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col">
+            <Card
+                title="Page"
+            >
+                {#snippet body()}
+        
+                    <h1>Page</h1>
+                    <p>This is a page.</p>
+        
+                {/snippet}
+            </Card>
+        </div>
+    </div>
+</div>
