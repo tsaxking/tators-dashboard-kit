7b4fe0a87faf29cad9361712c5ffdc5e71ea6d98
Taylor King <taylorking206@u.boisestate.edu>
2025-01-26 14:31:08 -0700

    init

---
+++ src/routes/dashboard/event/[eventKey]/strategy/[id]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Strategy } from "$lib/model/strategy";
+	import { onMount } from "svelte";
+	import type { TBAMatch } from "tatorscout-utils/tba";
+
+    interface Props {
+        matches: TBAMatch[];
+    }
+</script>
