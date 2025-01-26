88063fe4e262ff3a71d4c9e8fd1b4e338f51699f
Taylor King <taylorking206@u.boisestate.edu>
2025-01-26 14:01:13 -0700

    add datatable

--- src/lib/components/robot-display/TeamComments.svelte
+++ src/lib/components/robot-display/TeamComments.svelte
@@ -1,6 +1,31 @@
 <script lang="ts">
     import { FIRST } from "$lib/model/FIRST";
     import { Scouting } from "$lib/model/scouting";
+    import 'datatables.net-dt';
+	import { onMount } from "svelte";
 
-    interface Props {}
+
+    interface Props {
+        comments: {
+            account: string;
+            type: string;
+            comment: string;
+            match: string;
+        }[];
+    };
+
+    const { comments }: Props = $props();
+
+    let table: HTMLTableElement;
+
+    onMount(() => {
+        jQuery(table).DataTable({
+            data: comments,
+        });
+    });
 </script>
+
+<div class="table-responsive">
+    <table class="table table-striped" bind:this={table}>
+    </table>
+</div>
