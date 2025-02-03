b89d18fbf30f37f34a4185c6a12e8c953512da9c
Taylor King <taylorking206@u.boisestate.edu>
2025-02-03 12:21:49 -0700

    add file check for runTs

--- src/lib/server/utils/task.ts
+++ src/lib/server/utils/task.ts
@@ -2,10 +2,7 @@ import { attemptAsync } from "ts-utils/check";
 import { exec } from 'child_process';
 import path from 'path';
 import * as tsNode from 'ts-node';
-import { EventEmitter } from "ts-utils/event-emitter";
-import { pathToFileURL } from 'url';
-import { createRequire } from 'module';
-
+import fs from 'fs';
 
 export const runTask = (args: string[]) => {
     return attemptAsync(async () => new Promise<void>((res, rej) => exec(args.join(' '), (error) => {
@@ -21,6 +18,9 @@ export const runTs = async (file: string, fn: string, ...params: unknown[]) => {
         tsNode.register({
             transpileOnly: true,
         });
+        if (!fs.existsSync(path.resolve(process.cwd(), file))) {
+            throw new Error(`File ${file}.ts not found`);
+        }
         const mod = await import(path.resolve(process.cwd(), file));
         const func = mod[fn];
         if (!func) {
