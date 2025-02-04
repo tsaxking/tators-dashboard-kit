69f0b934b705be3cd5710c3411f219a0d50afa9d
Taylor King <taylorking206@u.boisestate.edu>
2025-02-04 06:49:09 -0700

    fix runTs

--- src/lib/server/utils/task.ts
+++ src/lib/server/utils/task.ts
@@ -1,8 +1,9 @@
 import { attemptAsync } from "ts-utils/check";
 import { exec } from 'child_process';
 import path from 'path';
-import * as tsNode from 'ts-node';
+// import * as tsNode from 'ts-node';
 import fs from 'fs';
+import url from 'url';
 
 export const runTask = (args: string[]) => {
     return attemptAsync(async () => new Promise<void>((res, rej) => exec(args.join(' '), (error) => {
@@ -15,20 +16,18 @@ export const runTask = (args: string[]) => {
 
 export const runTs = async (file: string, fn: string, ...params: unknown[]) => {
     return attemptAsync(async () => {
-        tsNode.register({
-            transpileOnly: true,
-        });
-        if (!fs.existsSync(path.resolve(process.cwd(), file))) {
-            throw new Error(`File ${file}.ts not found`);
-        }
-        const mod = await import(path.resolve(process.cwd(), file));
+        const fullpath = path.join(process.cwd(), file);
+
+        const mod = await import(url.pathToFileURL(fullpath).href);
         const func = mod[fn];
+
         if (!func) {
             throw new Error(`Function ${fn} not found in ${file}`);
         }
         if (typeof func !== 'function') {
             throw new Error(`${fn} is not a function`);
         }
+
         return (await func(...params));
     });
 };
