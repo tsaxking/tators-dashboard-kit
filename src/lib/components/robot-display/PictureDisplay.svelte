de15c37d207d7f44162106645d26289678b46cb9
Taylor King <taylorking206@u.boisestate.edu>
2025-02-03 19:51:09 -0700

    picture display template

---
+++ src/lib/components/robot-display/PictureDisplay.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { FIRST } from '$lib/model/FIRST';
+
+    interface Props {
+        team: FIRST.TeamsData;
+        pictures: FIRST.TeamPicturesArr;
+    }
+
+    const { team, pictures }: Props = $props();
+</script>
+
